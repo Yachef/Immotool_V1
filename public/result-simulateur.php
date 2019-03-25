43509bf49c324c132c926c92add04a46ba1bc981
Yacine <Yacine@yachef.h@gmail.com>
2019-03-25 18:12:47 +0900

    second commit

--- public/result-simulateur.php
+++ public/result-simulateur.php
@@ -30,6 +30,7 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="../css/style-app.css">
+    <link rel="stylesheet" href="../css/style-results.css">
 
     <!-- JS -->
     <script src = "../js/utils.js"></script>
@@ -47,165 +48,31 @@
 
                     <div class = "header-content">
                         <div class="title">
-                            <h1 id="title">Simulateur</h1>
+                            <h1 id="title">Resultats</h1>
+                            <div class = "return"><a href = "simulateur.php"><h2>Revenir au simulateur</h2></a></div>
                         </div>
                        <div class = "divider"><hr></div>
                     </div>
-                  
 
-                    <section id = "form" class = "content-wrapper">
-                        <div class="content-container">
-                        <h1>RESULTATS :</h1>
-                        <?php
-                          echo "fraisNotaire ".$_SESSION['fraisNotaire'];
-                          echo "<br>";
-                          echo "totalCoutAchat ".$_SESSION['totalCoutAchat'];
-                          echo "<br>";
-                          echo "<br>";
-
-                          echo "montantPret ".$_SESSION['montantPret'];
-                          echo "<br>";
-                          echo "mensualitesHorsAssurance ".$_SESSION['mensualitesHorsAssurance'];
-                          echo "<br>";
-                          echo "mensualitesTotales ". $_SESSION['mensualitesTotales'];
-                          echo "<br>";
-                          echo "remboursementAnnuel". $_SESSION['remboursementAnnuel'];
-                          echo "<br>";
-                          echo "interetEmprunt1ereAnnee ". $_SESSION['interetEmprunt1ereAnnee'];
-                          echo "<br>";
-                          echo "coutTotalCredit ". $_SESSION['coutTotalCredit'];
-                          echo "<br>";
-                          echo "<br>";
-
-                          echo "TMI ".$_SESSION['TMI'];
-                          echo "<br>";
-                          echo "<br>";
-
-                          echo "totalRecettesAnuelles ".$_SESSION['totalRecettesAnuelles'] ;
-                          echo "<br>";
-                          echo "<br>";
-                          
-                          echo "assuranceCredit ".$_SESSION['assuranceCredit'];
-                          echo "<br>";
-                          echo "totalChargesAnnuelles ".$_SESSION['totalChargesAnnuelles'];
-                          echo "<br>";
-                          echo "<br>";
-                          ?>
-                          <h1>Retour Sur Investissement </h1>
-                          <?php
-
-                          echo "Rendement Brut ".$_SESSION['rendementBrut'];
-                          echo "<br>";
-                          echo "Rendement Net". $_SESSION['rendementNet'];
-                          echo "<br>";
-                          echo "<br>";
+                    <section class = "col-md-4 content-wrapper">
+                    </section>
 
 
-                          echo "régime Micro BIC (LMNP) ".$_SESSION['regimeMicroBic'];
-                          echo "<br>";
-                          echo "impotsFoncierLMNP Micro Bic ".$_SESSION['impotsFoncierLMNPm'];
-                          echo "<br>";
-                          echo "cashflowLMNPm Micro Bic ".$_SESSION['cashflowLMNPm'];
-                          echo "<br>";
-                          echo "<br>";
+                    <section id = "rendement" class = "col-md-4 content-wrapper">
+                        <div class = "content-container">
+                            <div class = "content-title"><h1>Rendement</h1></div>
+                            <div style = "text-align:center;">
+                                <h2>Rendement Brut</h2>
+                                <h2 style = "color:red; font-size:30px;"><?php echo round($_SESSION['rendementBrut']*100,2)."%" ?></h2>
+                                <h2>Rendement Net (avant imp&ocirc;ts)</h2>
+                                <h2 style = "color:green; font-size:30px;"><?php echo round($_SESSION['rendementNet']*100,2)."%" ?></h2>
+                            </div>
 
-                          ?>
-                          <h1>Régime Micro Foncier en location nue</h1>
-                          <?php 
-                          echo "<br>";
-                          echo "Abattement de 30% ". $_SESSION['abattementmF'];
-                          echo "<br>";
-                          echo "Revenu Foncier Imposable ".$_SESSION['revenusFonciersImposablesmF'];
-                          echo "<br>";
-                          echo "Revenu Global Imposable ".$_SESSION['revenuGlobalmF'];
-                          echo "<br>";
-                          echo "Tranche Marginale d'imposition ".$_SESSION['newTMImF'];
-                          echo "<br>";
-                          echo "Impots Foncier ". $_SESSION['impotsFonciermF'];
-                          echo "<br>";
-                          echo "Revenus Net ".$_SESSION['revenus']; 
-                          echo "<br>";
-                          echo "cashflow ".$_SESSION['cashflowmF'];
-                          echo "<br>";
-                          echo "<br>";
-                            ?>
-                          <h1>Régime Réel (location vide)</h1>
-                          <?php
-                          echo "<br>";
-                          // echo "Recettes moins charges financières ". $_SESSION['recettesMoinsCharges'];
-                          // echo "<br>";
-                          // echo "Recettes moins charges dont travaux ".$_SESSION['recettesMoinsChargesDontTravaux'];
-                          // echo "<br>";
-                          // echo "Revenus Fonciers Imposable ".$_SESSION['revenusFonciersImposablesR'];
-                          // echo "<br>";
-                          // echo "Revenu Global Imposable ".$_SESSION['revenuGlobalR'];
-                          // echo "<br>";
-                          // echo "Tranche Marginale d'imposition ".$_SESSION['newTMIR'];
-                          echo "<br>";
-                          echo "Impots Foncier ". $_SESSION['impotsFoncierR'];
-                          echo "<br>";
-                          echo "Revenus Net ".$_SESSION['revenus']; 
-                          echo "<br>";
-                          echo "cashflow ".$_SESSION['cashflowR'];
-                          echo "<br>";
-
-                          ?>
-                          
-                          <h1>Régime Micro BIC en location meublée</h1>
-                          <?php
-                          echo "<br>";
-                          echo "Accès au micro-BIC ? ". $_SESSION['LMNPm'];
-                          echo "<br>";
-                          echo "Abattement de 50% ".$_SESSION['abattementLMNPm'];
-                          echo "<br>";
-                          echo "Revenus Fonciers Imposable ".$_SESSION['revenusFonciersImposablesLMNPm'];
-                          echo "<br>";
-                          echo "Revenu Global Imposable ".$_SESSION['revenuGlobalLMNPm'];
-                          echo "<br>";
-                          echo "Tranche Marginale d'imposition ".$_SESSION['newTMILMNPm'];
-                          echo "<br>";
-                          echo "Impots Foncier ". $_SESSION['impotsFoncierLMNPm'];
-                          echo "<br>";
-                          echo "Revenus Net ".$_SESSION['revenus']; 
-                          echo "<br>";
-                          echo "cashflow ".$_SESSION['cashflowLMNPm'];
-                          echo "<br>";
-                          ?>
-
-                          <h1>Régime Réel simplifié en location meublée</h1>
-                          <?php
-                          // echo "<br>";
-                          // echo "Part du foncier ". $_SESSION['PartDuFoncier'];
-                          // echo "<br>";
-                          // echo "Amortissement du mobilier (sur 10ans) ".$_SESSION['amortissementMobilier'];
-                          // echo "<br>";
-                          // echo "Amortissement des travaux (sur 10ans) ".$_SESSION['amortissementTravaux'];
-                          // echo "<br>";
-                          // echo "Amortissement de l'immobilier (sur 30 ans) ".$_SESSION['amortissementImmobilier'];
-                          // echo "<br>";
-                          // echo "Charges générées par l'amortissement ".$_SESSION['chargesAmortissement'];
-                          // echo "<br>";
-                          // echo "Recettes moins charges financières ".$_SESSION['recettesMoinsCharges'];
-                          // echo "<br>";
-                          // echo "  moins charges non financières ".$_SESSION['moinsChargesNonFinancieres'];
-                          // echo "<br>";
-                          // echo "  moins charges d'amortissement ".$_SESSION['moinsChargesAmortissement'];
-                          // echo "<br>";
-                          // echo "Revenu Foncier Imposable ".$_SESSION['revenusFonciersImposablesBIC'];
-                          // echo "<br>";
-                          // echo "Revenu Global Imposable ".$_SESSION['revenuGlobalBIC'];
-                          // echo "<br>";
-                          // echo "Tranche Marginale d'imposition ".$_SESSION['newTMIBIC'];
-                          echo "<br>";
-                          echo "Impots Foncier ". $_SESSION['impotsFoncierBIC'];
-                          echo "<br>";
-                          echo "Revenus Net ".$_SESSION['revenus']; 
-                          echo "<br>";
-                          echo "cashflow ".$_SESSION['cashflowBIC'];
-                          echo "<br>";
-                          echo "<br>";
-                          ?>
                         </div>
+                    </section>
+
+
+                    <section class = "col-md-4 content-wrapper">
                     </section>
 
                 </div>
